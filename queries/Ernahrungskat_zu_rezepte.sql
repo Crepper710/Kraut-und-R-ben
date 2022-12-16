7d16dd1e9988b839c577712a66f5b6015981da6c
Crepper710 <65467857+Crepper710@users.noreply.github.com>
2022-12-16 08:43:44 +0100

    Delete Ernahrungskat_zu_rezepte.sql

--- queries/Ernahrungskat_zu_rezepte.sql
+++
@@ -1,5 +0,0 @@
-SELECT REZEPT.REZEPTNAME
-FROM ernaehrungskategorien
-INNER JOIN ERNAEHRUNGKATREZEPT ON ernaehrungskategorien.Ernaehrungskategorie_id = ERNAEHRUNGKATREZEPT.Ernaehrungskategorie_id
-INNER JOIN REZEPT ON ERNAEHRUNGKATREZEPT.REZEPTNR = REZEPT.REZEPTNR
-WHERE ernaehrungskategorien.Ernaehrungskategoriename = '';
